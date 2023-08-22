09bb3feec1b390df9668ed11346b7f7c8b7a4912
Cherry-Ko-20 <echory20@gmail.com>
2023-08-22 14:36:18 -0700

    adding the app.py

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import streamlit as st 
 import plotly.express as px
+import numpy as np
 
-spotify_genre=pd.read_csv('./csvFiles/genres_v2.csv')
-spotify_playlist= pd.read_csv('./csvFiles/playlists.csv')
+spotify_genre=pd.read_csv('genres_v2.csv')
+spotify_playlist= pd.read_csv('playlists.csv')
 print(spotify_genre)
 print(spotify_playlist)
