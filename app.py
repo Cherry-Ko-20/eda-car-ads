46e7bcd4577a83eadf0d724b0015bae85730ee25
Cherry-Ko-20 <echory20@gmail.com>
2023-08-22 16:26:40 -0700

    added libraries and graphs

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import streamlit as st
 import plotly.express as px
 import numpy as np
 from PIL import Image 
+import scipy as sp
+import seaborn as sns 
 
 spotify_genre=pd.read_csv('genres_v2.csv')
 spotify_playlist= pd.read_csv('playlists.csv')
@@ -24,3 +26,34 @@ missing_playlist= spotify_playlist.isnull().sum()
 print(missing_playlist)
 spotify_genre= spotify_genre.drop(columns= ["Unnamed: 0"])
 print(spotify_genre)
+
+
+spotify_genre.hist(figsize=(12, 10))
+plt.tight_layout()
+plt.show()
+
+
+# Correlation heatmap
+correlation_matrix = spotify_genre.corr()
+sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm')
+plt.title('Correlation Heatmap')
+plt.show()
+
+# Countplot of genre distribution
+sns.countplot(data=spotify_genre, x='genre')
+plt.xticks(rotation=90)
+plt.title('Genre Distribution')
+plt.show()
+
+# Box plot of danceability by genre
+plt.figure(figsize=(12, 6))
+sns.boxplot(data=spotify_genre, x='genre', y='danceability')
+plt.xticks(rotation=90)
+plt.title('Danceability by Genre')
+plt.show()
+
+# Scatter plot of energy vs loudness
+plt.figure(figsize=(8, 6))
+sns.scatterplot(data=spotify_genre, x='energy', y='loudness', hue='genre')
+plt.title('Energy vs Loudness')
+plt.show()
