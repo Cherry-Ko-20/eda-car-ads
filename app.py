1e6cc08104e53b597f9fd031c86544d04f8ef415
Cherry-Ko-20 <echory20@gmail.com>
2023-08-23 14:00:30 -0700

    created hist and scatter plot graphs

--- app.py
+++ app.py
@@ -34,8 +34,34 @@ st.header('Spotify Genre Analysis')
 
 #Scatter plot or histogram based on checkbox value 
 st.subheader('Scatter PlotL: Danceability vs Energy')
-    scatter_fig = px.scatter(spotify_genre, x = 'danceability', y= 'energy', color='genre')
-    st.plotly_chart(scatter_fig)
+scatter_fig = px.scatter(spotify_genre, x = 'danceability', y= 'energy', color='genre')
+st.plotly_chart(scatter_fig)
+
 st.subheader('Histogram: Danceability')
-    histogram_fig= px.histogram(spotify_genre, x = 'danceability', color= 'genre', marginal='rug')
-    st.plotly_chart(histogram_fig)
+histogram_fig= px.histogram(spotify_genre, x = 'danceability', color= 'genre', marginal='rug')
+st.plotly_chart(histogram_fig)
+
+
+selected_genre = st.selectbox('Select Genre for Histogram', spotify_genre['genre'].unique())
+filtered_data = spotify_genre[spotify_genre['genre'] == selected_genre]
+
+# Histogram based on selected genre
+st.subheader(f'Histogram: Danceability for {selected_genre}')
+histogram_fig = px.histogram(filtered_data, x='danceability', marginal='rug')
+st.plotly_chart(histogram_fig)
+
+selected_genre_scatter = st.selectbox('Select Genre for Scatter Plot', spotify_genre['genre'].unique())
+filtered_data_scatter = spotify_genre[spotify_genre['genre'] == selected_genre_scatter]
+
+selected_columns = st.multiselect('Select Columns for Scatter Plot', spotify_genre.columns)
+
+if len(selected_columns) == 2:  # Only plot if exactly 2 columns are selected
+    scatter_data = filtered_data_scatter[selected_columns]
+    x_column, y_column = selected_columns
+    
+    # Scatter plot
+    st.subheader(f'Scatter Plot: {x_column} vs {y_column} for {selected_genre_scatter}')
+    scatter_fig = px.scatter(scatter_data, x=x_column, y=y_column)
+    st.plotly_chart(scatter_fig)
+else:
+    st.warning('Select exactly 2 columns for the scatter plot')
